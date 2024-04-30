0d35a128e1bf4f4d5654d58118e1ebd973acb9f4
Diego Cordeiro <diegocordeiro121@gmail.com>
2024-04-30 17:15:15 -0300

    feat: começando implementação da lógica da aplicação

--- src/components/CurrentPrice/index.jsx
+++ src/components/CurrentPrice/index.jsx
@@ -1,10 +1,27 @@
 import { Text, View } from 'react-native'
 import styles from './styles'
+import { useEffect, useState } from 'react'
 
 export default function CurrentPrice() {
+  const [currentPrice, setData] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const currentData = await fetch(
+          'https://economia.awesomeapi.com.br/BTC-BRL/1',
+        {method: 'GET'});
+        const currentJson = await currentData.json()
+        setData(parseFloat(currentJson[0].ask) / 1000)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [])
   return (
     <View style={styles.currentContainer}>
-      <Text style={styles.currentPrice}>$ 4555.665</Text>
+      <Text style={styles.currentPrice}>R$ {currentPrice}</Text>
       <Text style={styles.currentText}>ultima cotação</Text>
     </View>
   )
